22e4d7739b83354dfd54a6754f9f9b8d7a05aead
BartoszNowak <74794831+Bartosz-Nowak05@users.noreply.github.com>
2026-02-06 21:05:18 +0100

    Update db_connect.php

--- Projekty/Projekt drużyny piłkarskiej/db_connect.php
+++ Projekty/Projekt drużyny piłkarskiej/db_connect.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "piłkarze";
+$db = "pilkarze";
 $conn = new mysqli($host,$user,$pass,$db);
 if ($conn->connect_error) {
     echo "Błąd połączenia: " . $conn->connect_error;
